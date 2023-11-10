05c2d3386b52e73d6631c823d2747c7cf6c77ad4
adrocx <adiraju.raju@gmail.com>
2023-11-10 16:31:54 +0530

    “hello”

---
+++ mymath.py
@@ -0,0 +1,9 @@
+def subtract(a: int, b: int) -> int:
+    """
+    Return the result of subtracting b from a.
+
+    :param a: The minuend.
+    :param b: The subtrahend.
+    :return: The result of the subtraction.
+    """
+    return a - b
